ab2b3c51019b6fd4e26134cf5b5f90359e0f767d
Tristan Schwörer <tschwo21@student.aau.dk>
2022-03-29 17:03:04 +0200

    Correct library name

--- JetsonWorkspace/src/people_detection/setup.py
+++ JetsonWorkspace/src/people_detection/setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['librealsense2'],
+    packages=['pyrealsense2'],
     package_dir={'': 'src'},
 )
 
