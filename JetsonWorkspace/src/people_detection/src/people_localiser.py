6f9039c767b5e0bd8036b36bc1961aa82e3748a7
Jonathan Eichild Schmidt <33545029+JonathanESchmidt@users.noreply.github.com>
2022-03-31 13:58:30 +0200

    Update people_localiser.py

--- JetsonWorkspace/src/people_detection/src/people_localiser.py
+++ JetsonWorkspace/src/people_detection/src/people_localiser.py
@@ -136,7 +136,7 @@ class PeopleLocaliser():
         frameid = None #Stand-in until added to capture image
         colour, depth = self.captureImages()#TODO call find depth function
         detections = None
-        if type(colour) != None:
+        if colour.any() != None:
             if self.networkname == "ssd-mobilenet-v2":
                 people = self.detectSSD(colour, depth)
 
