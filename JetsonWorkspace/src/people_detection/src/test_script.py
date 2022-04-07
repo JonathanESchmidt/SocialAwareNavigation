20008eaaf2d217e28b28b1387500905d96d1a31d
Jonathan Eichild Schmidt <33545029+JonathanESchmidt@users.noreply.github.com>
2022-04-07 15:17:18 +0200

    plus this!

--- JetsonWorkspace/src/people_detection/src/test_script.py
+++ JetsonWorkspace/src/people_detection/src/test_script.py
@@ -221,7 +221,7 @@ class PeopleLocaliser():
             bottom=detection.Bottom
 
             cv2.rectangle(image, (int(left), int(top)), (int(right), int(bottom)), (0, 255, 0), 3)
-            cv2.putText(image, "Polar: " +str(angle) + "deg, " str(distance) + "m", (100, 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.putText(image, "Polar: " +str(angle) + "deg, " + str(distance) + "m", (100, 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             cv2.putText(image, "Cartesian: " +str(x) + "X, " + str(y) + "Y", (200, 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
         cv2.putText(image, "FPS: " + str(framerate), (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
